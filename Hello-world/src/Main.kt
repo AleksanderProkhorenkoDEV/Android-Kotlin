f176a3ef88311b4e2b444e0e6906d7dd71fd0278
aleksander <aprokhorenkodev@gmail.com>
2025-09-30 19:22:50 +0200

    feat: declare two variable and display his content

--- Hello-world/src/Main.kt
+++ Hello-world/src/Main.kt
@@ -1,5 +1,12 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-   println("Hello Kotlin")
+    println("Hello Kotlin")
+
+    //Declare variables
+    val message = "This is read variable only"
+    var number = 4
+
+    println(message)
+    println(number)
 }
