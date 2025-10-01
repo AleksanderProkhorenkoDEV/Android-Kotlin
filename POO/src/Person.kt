4361c312d28a7d58887ec58580261d7e9b08ec83
Aleksander <aprokhorenkodev@gmail.com>
2025-10-01 20:57:58 +0200

    refactor: delete duplicate function

--- POO/src/Person.kt
+++ POO/src/Person.kt
@@ -11,12 +11,6 @@ class Person(var name: String, var lastName: String, var age: Int) {
     val isAdult: Boolean
         get() = age >= 18
 
-    fun setEmail(email: String): Boolean {
-        if (email.isEmpty() || email.isBlank()) throw error("Email cannot be blank")
-        this.email = email
-        return true
-    }
-
     fun getPerson(): String {
         return "Hi, I am ${this.name}, I am ${this.age} years old, my email is ${this.email}"
     }
