344085c779ab683e9c49d538d484a8e24afb4560
Aleksander <aprokhorenkodev@gmail.com>
2025-10-03 15:12:15 +0200

    fix: error with the outside word

--- Android-Exercises/Movie-ticked-price/src/Main.kt
+++ Android-Exercises/Movie-ticked-price/src/Main.kt
@@ -6,7 +6,7 @@ In the initial code provided in the following code snippet, write a program that
 A children's ticket price of $15 for people 12 years old or younger.
 A standard ticket price of $30 for people between 13 and 60 years old. On Mondays, discount the standard ticket price to $25 for this same age group.
 A senior ticket price of $20 for people 61 years old and older. Assume that the maximum age of a moviegoer is 100 years old.
-A -1 value to indicate that the price is invalid when a user inputs an age outside of the age specifications.
+A -1 value to indicate that the price is invalid when a user inputs an age outside the age specifications.
 
  */
 
