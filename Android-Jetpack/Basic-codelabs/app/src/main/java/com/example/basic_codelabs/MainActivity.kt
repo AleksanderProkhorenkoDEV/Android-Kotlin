089fe6b13037cba1794e128cd3bb0eca3d210092
aleksander <aprokhorenkodev@gmail.com>
2025-10-06 20:09:10 +0200

    feat: add new funcionability to hidden and display diferentes components

--- Android-Jetpack/Basic-codelabs/app/src/main/java/com/example/basic_codelabs/MainActivity.kt
+++ Android-Jetpack/Basic-codelabs/app/src/main/java/com/example/basic_codelabs/MainActivity.kt
@@ -5,18 +5,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,19 +43,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp (modifier: Modifier = Modifier, names: List<String> = listOf("iOS", "Android")){
-    Surface(color = MaterialTheme.colorScheme.background,
-        modifier = modifier.padding(4.dp)
-    )
-    {
-        Column(modifier = Modifier.padding(vertical = 4.dp)) {
-            for (name in names){
-                Greeting(name)
-            }
+fun MyApp (modifier: Modifier = Modifier, ){
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    Surface(modifier){
+        if(shouldShowOnboarding){
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        }else{
+            Greetings()
         }
+    }
+
+
+}
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    BasiccodelabsTheme {
+        MyApp(Modifier.fillMaxSize())
     }
 }
 
+
+@Composable
+private fun Greetings(modifier: Modifier = Modifier, names: List<String> = listOf("iOS", "Android")){
+ Column(modifier = modifier.padding(vertical = 4.dp)) {
+     for(name in names){
+         Greeting(name)
+     }
+ }
+}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
 
@@ -83,6 +106,32 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     BasiccodelabsTheme {
-        MyApp()
+        Greetings()
+    }
+}
+
+@Composable
+fun OnboardingScreen(modifier: Modifier = Modifier, onContinueClicked: () -> Unit,) {
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    BasiccodelabsTheme {
+        OnboardingScreen(onContinueClicked = {})
     }
 }
