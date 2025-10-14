1e1d576c4d72afefd8d59e47e5991322d3c43f84
Aleksander <aprokhorenkodev@gmail.com>
2025-10-14 12:09:19 +0200

    refactor: update the state and make elevation of state, and make waterCounter without state

--- Android-Jetpack/BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/component/WaterCounter.kt
+++ Android-Jetpack/BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/component/WaterCounter.kt
@@ -1,16 +1,13 @@
 package com.example.basicstatecodelab.component
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -18,11 +15,14 @@ import androidx.compose.ui.unit.dp
 import com.example.basicstatecodelab.R
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
+fun WaterCounter(
+    count: Int,
+    onIncrement: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     Column(
         modifier = modifier.padding(16.dp, top = 24.dp)
     ) {
-        var count by remember { mutableIntStateOf(0) }
 
         if (count > 0) {
             Text(
@@ -30,9 +30,8 @@ fun WaterCounter(modifier: Modifier = Modifier) {
             )
         }
 
-
         Button(
-            onClick = { count++ },
+            onClick = onIncrement,
             modifier = Modifier.padding(8.dp),
             enabled = count < 10
         ) {
--- Android-Jetpack/BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/screen/WellNessCreen.kt
+++ Android-Jetpack/BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/screen/WellNessCreen.kt
@@ -1,10 +1,19 @@
 package com.example.basicstatecodelab.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.example.basicstatecodelab.component.WaterCounter
 
 @Composable
-fun WellnessScreen(modifier: Modifier = Modifier){
-    WaterCounter(modifier)
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableIntStateOf(0) }
+    WaterCounter(
+        count,
+        { count++ },
+        modifier
+    )
 }
