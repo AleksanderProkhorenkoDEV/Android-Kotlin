859763d4e236e7a09a77f94cee23f77ff9df23b0
Aleksander <aprokhorenkodev@gmail.com>
2025-10-16 11:36:12 +0200

    feat: add the function and make private, and create a list with mutableState

--- Android-Jetpack/BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/screen/WellNessCreen.kt
+++ Android-Jetpack/BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/screen/WellNessCreen.kt
@@ -4,15 +4,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.example.basicstatecodelab.component.WaterCounter
 import com.example.basicstatecodelab.component.WellnessTaskList
+import com.example.basicstatecodelab.model.WellnessTask
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
     var count by rememberSaveable { mutableIntStateOf(0) }
+    val list = remember {
+        mutableStateListOf<WellnessTask>().apply { addAll(getWellnessTasks()) }
+    }
     Column(
         modifier = modifier
     ) {
@@ -21,6 +27,8 @@ fun WellnessScreen(modifier: Modifier = Modifier) {
             { count++ },
             modifier
         )
-        WellnessTaskList()
+        WellnessTaskList(list, onCloseTask = { task -> list.remove(task)})
     }
 }
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
