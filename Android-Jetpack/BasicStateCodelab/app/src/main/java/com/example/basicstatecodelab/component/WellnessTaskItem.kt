71eb27f4bda6a2c3f1663f253ea1cc80390c4f18
Aleksander <aprokhorenkodev@gmail.com>
2025-10-16 11:19:43 +0200

    feat: Override a composable function to use the first and elevate the state

--- Android-Jetpack/BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/component/WellnessTaskItem.kt
+++ Android-Jetpack/BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/component/WellnessTaskItem.kt
@@ -4,10 +4,15 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,7 +21,25 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WellnessTaskItem(
     taskName: String,
+    modifier: Modifier = Modifier
+) {
+    var checked by rememberSaveable { mutableStateOf(false) }
+
+    WellnessTaskItem(
+        taskName,
+        {},
+        checked = checked,
+        { checked = !checked},
+        modifier
+    )
+}
+
+@Composable
+fun WellnessTaskItem(
+    taskName: String,
     onClose: () -> Unit,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
     modifier: Modifier = Modifier
 ) {
     Row(
@@ -29,6 +52,10 @@ fun WellnessTaskItem(
                 .padding(start = 16.dp),
             text = taskName
         )
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
+        )
         IconButton(onClick = onClose) {
             Icon(
                 Icons.Filled.Close,
@@ -40,27 +67,12 @@ fun WellnessTaskItem(
 
 @Preview()
 @Composable
-fun WellnessTaskItemPreview(
-    modifier: Modifier = Modifier,
-    taskName: String = "Test preview",
-    onClose: () -> Unit = {},
-) {
-    Row(
-        modifier = modifier,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
-            modifier = Modifier
-                .weight(1f)
-                .padding(start = 16.dp),
-            text = taskName
-        )
-        IconButton(onClick = onClose) {
-            Icon(
-                Icons.Filled.Close,
-                contentDescription = "Close"
-            )
-        }
-    }
+fun WellnessTaskItemPreview() {
+    WellnessTaskItem(
+        taskName = "Test task",
+        {},
+        false,
+        {}
+    )
 }
 
