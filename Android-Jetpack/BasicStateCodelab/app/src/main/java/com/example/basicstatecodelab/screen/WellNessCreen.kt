91ce0d5dbe429a28955199aa1306a18b8c468996
Aleksander <aprokhorenkodev@gmail.com>
2025-10-16 11:19:56 +0200

    feat: call the new composable function

--- Android-Jetpack/BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/screen/WellNessCreen.kt
+++ Android-Jetpack/BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/screen/WellNessCreen.kt
@@ -1,5 +1,6 @@
 package com.example.basicstatecodelab.screen
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -7,13 +8,19 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.example.basicstatecodelab.component.WaterCounter
+import com.example.basicstatecodelab.component.WellnessTaskList
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
     var count by rememberSaveable { mutableIntStateOf(0) }
-    WaterCounter(
-        count,
-        { count++ },
-        modifier
-    )
+    Column(
+        modifier = modifier
+    ) {
+        WaterCounter(
+            count,
+            { count++ },
+            modifier
+        )
+        WellnessTaskList()
+    }
 }
