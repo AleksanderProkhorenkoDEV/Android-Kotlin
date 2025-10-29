8bda2b606d8e76fbaa47d69638cd5fff7e2020e7
Aleksander <aprokhorenkodev@gmail.com>
2025-10-29 13:14:33 +0100

    feat: create main activity

---
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.first_movile_app
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.first_movile_app.components.MainLayout
+import com.example.first_movile_app.ui.theme.FirstmovileappTheme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FirstmovileappTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    MainLayout()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FirstmovileappTheme {
+        Greeting("Android")
+    }
+}
+
