fedc34bc48899073cbbf4029aa5a6f041b6c6aa9
aleksander <aprokhorenkodev@gmail.com>
2025-10-31 18:39:36 +0100

    build: add new atritubes to the class

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/data/Task.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/data/Task.kt
@@ -3,7 +3,9 @@ package com.example.first_movile_app.data
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import java.sql.Date
+import java.time.LocalDate
 
-class Task(val id: Long, var text: String, initialCheck: Boolean = false){
+class Task(val id: Long, var text: String, var description: String, var endDate: LocalDate, initialCheck: Boolean = false){
     var checked by mutableStateOf(initialCheck)
 }
