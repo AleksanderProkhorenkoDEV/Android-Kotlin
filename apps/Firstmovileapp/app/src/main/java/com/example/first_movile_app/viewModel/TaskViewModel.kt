5c9e863ef45e398759a2efc2ce764cd6e9d73643
aleksander <aprokhorenkodev@gmail.com>
2025-10-31 18:41:49 +0100

    refactor: add the new props in the listOf

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/viewModel/TaskViewModel.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/viewModel/TaskViewModel.kt
@@ -1,12 +1,16 @@
 package com.example.first_movile_app.viewModel
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModel
 import com.example.first_movile_app.data.Task
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import java.time.LocalDate
 
 
+@RequiresApi(Build.VERSION_CODES.O)
 class TaskViewModel : ViewModel() {
     private val _task = MutableStateFlow<List<Task>>(emptyList())
     val tasks: StateFlow<List<Task>>
@@ -14,10 +18,10 @@ class TaskViewModel : ViewModel() {
 
     init {
         _task.value = listOf<Task>(
-            Task(id = 1, text = "Finish the App"),
-            Task(id = 2, text = "Learn compose"),
-            Task(id = 3, text = "make my first mobile app"),
-            Task(id = 4, text = "Became a senior developer"),
+            Task(id = 1, text = "Finish the App", description = "a lot of text", endDate = LocalDate.now()),
+            Task(id = 2, text = "Learn compose", description = "a lot of text", endDate = LocalDate.now()),
+            Task(id = 3, text = "make my first mobile app", description = "a lot of text", endDate = LocalDate.now()),
+            Task(id = 4, text = "Became a senior developer", description = "a lot of text", endDate = LocalDate.now()),
         )
     }
 
