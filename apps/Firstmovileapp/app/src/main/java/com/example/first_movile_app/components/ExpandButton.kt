9834bc4f26e0ece2a79db13ca08ad4e5523d940f
aleksander <aprokhorenkodev@gmail.com>
2025-10-31 18:39:55 +0100

    feat: create new component to examen the task

---
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/components/ExpandButton.kt
@@ -0,0 +1,30 @@
+package com.example.first_movile_app.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ExpandButton(
+    isExpanded: Boolean = false,
+    callbackExpanded: (Boolean) -> Unit
+){
+    IconButton(
+        onClick = { callbackExpanded(isExpanded) },
+    ){
+        Icon(
+            imageVector = if(isExpanded) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
+            contentDescription = "Expand"
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ExpandButtonPreview(){
+    ExpandButton(callbackExpanded = {})
+}
