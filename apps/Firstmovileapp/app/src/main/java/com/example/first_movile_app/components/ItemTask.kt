4a3f48b7c0ee052a33eacd6384e67681ea278869
aleksander <aprokhorenkodev@gmail.com>
2025-10-31 18:40:52 +0100

    feat: re-organice the component and make a new section

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/components/ItemTask.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/components/ItemTask.kt
@@ -1,15 +1,26 @@
 package com.example.first_movile_app.components
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
@@ -21,18 +32,24 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.first_movile_app.R
+import com.example.first_movile_app.data.Task
+import java.time.LocalDate
 
 @Composable
 fun ItemTask(
-    text: String,
+    task: Task,
     isChecked: Boolean,
     callbackChecked: (Boolean) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Row(
+
+    var isExpanded by rememberSaveable { mutableStateOf(false) }
+
+    Column(
         modifier = modifier
             .padding(8.dp)
             .fillMaxWidth(1f)
+            .animateContentSize()
             .shadow(
                 elevation = 4.dp,
                 shape = RoundedCornerShape(8.dp),
@@ -42,21 +59,48 @@ fun ItemTask(
                 color = MaterialTheme.colorScheme.surface,
                 shape = RoundedCornerShape(8.dp)
             ),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
-        Text(
-            text = text,
-            modifier = Modifier.padding(horizontal = 8.dp),
-        )
-        CheckButton(isChecked, callbackChecked = callbackChecked)
+        Row(
+            modifier = modifier
+                .fillMaxWidth(1f),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = task.text,
+                modifier = Modifier.padding(horizontal = 8.dp),
+            )
+            Row {
+                CheckButton(isChecked, callbackChecked = callbackChecked)
+                ExpandButton(isExpanded, callbackExpanded = { isExpanded = !isExpanded })
+            }
+        }
+        if(isExpanded){
+            Column(
+                modifier = Modifier
+                            .padding(8.dp),
+            ) {
+                Text(task.description)
+                Spacer(modifier = Modifier.height(8.dp))
+                Text("The end date is: ${task.endDate}")
+            }
+        }
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Preview(showBackground = true)
 @Composable
 fun ItemTaskPreview(
     modifier: Modifier = Modifier
 ) {
-    ItemTask("Test task", true, {}, modifier)
+    val task = Task(5, "Exam", "Exam of Android, you will make a Task app", LocalDate.now())
+    ItemTask(
+        task = task,
+        isChecked = true,
+        callbackChecked = {},
+        modifier = modifier
+    )
 }
