d6cb628ab307681a63c9b6429f0f80c5f8e334ef
aleksander <aprokhorenkodev@gmail.com>
2025-10-31 18:40:20 +0100

    refactor: change the prop passed to ItemTask

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/components/ItemListColumn.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/components/ItemListColumn.kt
@@ -1,43 +1,55 @@
 package com.example.first_movile_app.components
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.first_movile_app.data.Task
+import java.time.LocalDate
 
 @Composable
 fun ItemListColumn(
     tasks: List<Task>,
     callbackChangeStatus: (Task, Boolean) -> Unit,
     modifier: Modifier = Modifier,
-){
+) {
+
     LazyColumn(
         modifier = modifier,
     ) {
         items(
             items = tasks,
-            key = { task -> task.id}
-        ){taskItem ->
+            key = { task -> task.id }
+        ) { taskItem ->
             ItemTask(
-                text = taskItem.text,
+                task = taskItem,
                 isChecked = taskItem.checked,
-                callbackChecked = { checked -> callbackChangeStatus(taskItem, checked) }
+                callbackChecked = { checked -> callbackChangeStatus(taskItem, checked) },
             )
         }
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Preview(showBackground = true)
 @Composable
-fun ItemListColumnPreview(){
+fun ItemListColumnPreview() {
     val tasks = listOf<Task>(
-        Task(id = 1, text = "Finish the App"),
-        Task(id = 2, text = "Learn compose"),
-        Task(id = 3, text = "make my first mobile app"),
-        Task(id = 4, text = "Became a senior developer"),
+        Task(id = 1, text = "Finish the App", description = "a lot of text", endDate = LocalDate.now()),
+        Task(id = 2, text = "Learn compose", description = "a lot of text", endDate = LocalDate.now()),
+        Task(id = 3, text = "make my first mobile app", description = "a lot of text", endDate = LocalDate.now()),
+        Task(id = 4, text = "Became a senior developer", description = "a lot of text", endDate = LocalDate.now()),
     )
 
-    ItemListColumn(tasks = tasks, {tasks, checked -> true})
+    ItemListColumn(
+        tasks = tasks,
+        callbackChangeStatus = { tasks, checked -> true },
+    )
 }
