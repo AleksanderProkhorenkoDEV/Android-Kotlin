051080a079617668f70d14e3b8e618d0b69501d5
Aleksander <aprokhorenkodev@gmail.com>
2025-11-02 15:18:39 +0100

    try: to make the persist data, but the databa inspector dont work

--- codelabs/Android-Jetpack/basic-android-kotlin-compose-training-inventory-app-starter/app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
+++ codelabs/Android-Jetpack/basic-android-kotlin-compose-training-inventory-app-starter/app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
@@ -1,12 +1,14 @@
 package com.example.inventory.data
 
 import android.content.Context
+import android.util.Log
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Item::class], version = 1, exportSchema = false)
 abstract class InventoryDatabase : RoomDatabase() {
+
     abstract fun itemDao(): ItemDao
 
     companion object {
@@ -14,15 +16,15 @@ abstract class InventoryDatabase : RoomDatabase() {
         private var Instance: InventoryDatabase? = null
 
         fun getDatabase(context: Context): InventoryDatabase {
+            Log.d("db","initialize function running")
 
-            return Instance ?: synchronized(this)
-            {
-                Room.databaseBuilder(context, InventoryDatabase::class.java, "item_database")
+            if (Instance != null) Log.d("db", "database exists") else Log.d("db", "doesnt exist")
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, InventoryDatabase::class.java, "inventory")
+                    .fallbackToDestructiveMigration()
                     .build()
-                    .also { Instance = it}
+                    .also { Instance = it }
             }
         }
-
-
     }
 }
--- codelabs/Android-Jetpack/basic-android-kotlin-compose-training-inventory-app-starter/app/src/main/java/com/example/inventory/ui/theme/Theme.kt
+++ codelabs/Android-Jetpack/basic-android-kotlin-compose-training-inventory-app-starter/app/src/main/java/com/example/inventory/ui/theme/Theme.kt
@@ -116,7 +116,6 @@ fun InventoryTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
             WindowCompat
                 .getInsetsController(window, view)
                 .isAppearanceLightStatusBars = darkTheme
