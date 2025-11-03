e1d49642f80d9b3df279c99edba5934884825aeb
aleksander <aprokhorenkodev@gmail.com>
2025-11-03 18:38:48 +0100

    feat: create the entity

---
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/dataBase/entities/Task.kt
@@ -0,0 +1,13 @@
+package com.example.first_movile_app.dataBase.entities
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalDate
+@Entity(tableName = "tasks")
+data class Task(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    var text: String,
+    var description: String,
+    var endDate: LocalDate, //TODO: make a Typeconvert
+    var isChecked: Boolean)
