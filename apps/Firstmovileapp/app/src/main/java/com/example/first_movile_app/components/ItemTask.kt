551c6a713325940da1f7a43d153fc36213e35ae9
aleksander <aprokhorenkodev@gmail.com>
2025-11-03 18:38:12 +0100

    feat: update the import of entity

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/components/ItemTask.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/components/ItemTask.kt
@@ -24,7 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.first_movile_app.dataBase.Task
+import com.example.first_movile_app.dataBase.entities.Task
 import java.time.LocalDate
 
 @Composable
