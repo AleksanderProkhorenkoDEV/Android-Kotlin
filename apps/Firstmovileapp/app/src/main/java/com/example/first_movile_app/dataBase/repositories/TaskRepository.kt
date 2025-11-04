0d27a19fdbec408095580b039685c99345de8313
Aleksander <aprokhorenkodev@gmail.com>
2025-11-04 22:49:05 +0100

    refactor: fix the error in the file name

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/dataBase/repositories/TaskRepository.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/dataBase/repositories/TaskRepository.kt
@@ -1,8 +1,13 @@
 package com.example.first_movile_app.dataBase.repositories
 
 import com.example.first_movile_app.dataBase.entities.Task
+import kotlinx.coroutines.flow.Flow
 
-interface TaskRespository {
+interface TaskRepository{
 
     suspend fun insertTask(task: Task)
+
+    fun getAllTask(): Flow<List<Task>>
+
+    suspend fun updateChecked(task: Task)
 }
