3a8199747f4940fe1a8eb0072641edac9fee55e2
Aleksander <aprokhorenkodev@gmail.com>
2025-11-04 22:51:06 +0100

    feat: add  the logic to update the check status and delete the hard code list task

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/theme/viewModel/TaskViewModel.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/theme/viewModel/TaskViewModel.kt
@@ -1,33 +1,27 @@
 package com.example.first_movile_app.ui.theme.viewModel
 
-import android.os.Build
-import androidx.annotation.RequiresApi
+
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.first_movile_app.dataBase.entities.Task
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import java.time.LocalDate
+import com.example.first_movile_app.dataBase.repositories.TaskRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 
 
-@RequiresApi(Build.VERSION_CODES.O)
-class TaskViewModel : ViewModel() {
-    private val _task = MutableStateFlow<List<Task>>(emptyList())
-    val tasks: StateFlow<List<Task>>
-        get() = _task.asStateFlow()
+class TaskViewModel(
+    private val taskRepository: TaskRepository
+) : ViewModel() {
 
-    init {
-        _task.value = listOf<Task>(
-            Task(id = 1, text = "Finish the App", description = "a lot of text", endDate = LocalDate.now()),
-            Task(id = 2, text = "Learn compose", description = "a lot of text", endDate = LocalDate.now()),
-            Task(id = 3, text = "make my first mobile app", description = "a lot of text", endDate = LocalDate.now()),
-            Task(id = 4, text = "Became a senior developer", description = "a lot of text", endDate = LocalDate.now()),
-        )
-    }
+    val tasks: Flow<List<Task>> = taskRepository.getAllTask()
 
-    fun changeTaskStatus(item: Task, checked: Boolean) {
-        _task.value.find { it.id == item.id }?.let{ task ->
-            task.checked = checked
+    fun updateChecked(
+        task: Task,
+        isChecked: Boolean
+    ){
+        viewModelScope.launch {
+            val updateTask = task.copy(isChecked= isChecked)
+            taskRepository.updateChecked(updateTask)
         }
     }
 }
