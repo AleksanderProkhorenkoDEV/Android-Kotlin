ed868cb5d4b33752bb4d5a204e97af17afc9bd31
Aleksander <aprokhorenkodev@gmail.com>
2025-11-07 13:33:34 +0100

    feat: add a global val, to pass to the bottomBar

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/navigation/TaskDestination.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/navigation/TaskDestination.kt
@@ -1,10 +1,24 @@
 package com.example.first_movile_app.navigation
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.ui.graphics.vector.ImageVector
+
 
 sealed class TaskDestination(
     val route: String,
-    val title: String
+    val title: String,
+    val icon: ImageVector
 ) {
-    object TaskList : TaskDestination("task_list", "ToDo List")
-    object CreateTask : TaskDestination("create_task", "Create new Task")
+    object TaskList : TaskDestination("task_list", "ToDo List", Icons.Default.Home)
+    object CreateTask : TaskDestination("create_task", "Create new Task", Icons.Default.AddCircle)
+    object AccountSettings : TaskDestination("account_settings", "Settings", Icons.Default.Settings)
 }
+
+val taskDestinationBottomBar = listOf(
+    TaskDestination.TaskList,
+    TaskDestination.CreateTask,
+    TaskDestination.AccountSettings
+)
