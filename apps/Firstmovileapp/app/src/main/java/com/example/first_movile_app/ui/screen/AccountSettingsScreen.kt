4e42c26f7878d667de398717f5d4172ae3585751
Aleksander <aprokhorenkodev@gmail.com>
2025-11-07 13:32:47 +0100

    feat: create new screen

---
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/screen/AccountSettingsScreen.kt
@@ -0,0 +1,9 @@
+package com.example.first_movile_app.ui.screen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AccountSettingsScreen () {
+    Text("Settings")
+}
