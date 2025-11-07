af7b3791a3bcb6dd965936019312dc269d81fffd
Aleksander <aprokhorenkodev@gmail.com>
2025-11-07 12:43:25 +0100

    style: clean the imports and add requiresApi

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/MainActivity.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/MainActivity.kt
@@ -1,21 +1,16 @@
 package com.example.first_movile_app
 
-import android.annotation.SuppressLint
+
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.first_movile_app.ui.theme.screen.MainLayout
-import com.example.first_movile_app.ui.theme.FirstmovileappTheme
+
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
