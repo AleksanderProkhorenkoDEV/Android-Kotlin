95db67336c51c8e593603c191d18820b1f898c2a
Aleksander <aprokhorenkodev@gmail.com>
2025-11-11 17:36:39 +0100

    feat: add the button and call the view model to add onClick and loading state

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/screen/CreateTaskScreen.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/screen/CreateTaskScreen.kt
@@ -6,39 +6,66 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.first_movile_app.R
 import com.example.first_movile_app.ui.components.InputLabel
 import com.example.first_movile_app.ui.components.TextFieldCustom
+import com.example.first_movile_app.viewModel.CreateTaskViewModel
+import com.example.first_movile_app.viewModel.ViewModalContainer
 
 @Composable
-fun CreateTaskScreen(modifier: Modifier = Modifier) {
-    Box(modifier = modifier
-        .fillMaxSize()
-        .padding(16.dp)
+fun CreateTaskScreen(
+    modifier: Modifier = Modifier,
+    viewModel: CreateTaskViewModel = viewModel(factory = ViewModalContainer.Factory)
+) {
+    val uiState = viewModel.uiState.collectAsState()
+
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
     ) {
         Text(text = stringResource(R.string.create_task_title_form))
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-            ) {
+        ) {
             TextFieldCustom(
-                value = "name",
-                onValueChange = {},
-                placeholder = "Name of the task",
-                inputLabel = { InputLabel("Name") })
+                value = uiState.value.name,
+                onValueChange = { newName -> viewModel.onChangeName(newName) },
+                placeholder = stringResource(R.string.create_form_name_placeholder),
+                inputLabel = { InputLabel(value = stringResource(R.string.create_form_name_label)) },
+                errorList = uiState.value.nameError
+                )
             Spacer(modifier = Modifier.padding(16.dp))
             TextFieldCustom(
-                value = "description",
-                onValueChange = {},
-                placeholder = "Description of the task",
-                inputLabel = { InputLabel("Description") })
+                value = uiState.value.description,
+                onValueChange = { newDescription -> viewModel.onChangeDescription(newDescription) },
+                placeholder = stringResource(R.string.create_form_description_placeholder),
+                inputLabel = { InputLabel(value = stringResource(R.string.create_form_description_label)) },
+                errorList = uiState.value.descriptionError
+            )
+            Spacer(modifier = Modifier.padding(16.dp))
+            Button(
+                onClick = { viewModel.saveTask() },
+                enabled = !uiState.value.isLoading
+            ) {
+                if(uiState.value.isLoading){
+                    Text(text = stringResource(R.string.loading_button))
+                }else{
+                    Text(text = stringResource(R.string.create_task_button))
+                }
+            }
         }
+
     }
 }
 
