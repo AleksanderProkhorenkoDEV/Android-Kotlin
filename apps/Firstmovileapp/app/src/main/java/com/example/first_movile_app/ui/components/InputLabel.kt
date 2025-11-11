746c16c9029221a12351e06dce5b8111ec3720eb
Aleksander <aprokhorenkodev@gmail.com>
2025-11-11 11:38:04 +0100

    feat: create a custom label

---
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/components/InputLabel.kt
@@ -0,0 +1,9 @@
+package com.example.first_movile_app.ui.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun InputLabel(value: String){
+    Text(value)
+}
