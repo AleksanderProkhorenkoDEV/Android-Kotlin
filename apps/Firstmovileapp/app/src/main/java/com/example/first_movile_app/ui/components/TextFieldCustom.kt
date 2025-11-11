5aa231ecfa11b8a02c6509228a697ce6a406a1a2
Aleksander <aprokhorenkodev@gmail.com>
2025-11-11 17:36:49 +0100

    feat: add error message

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/components/TextFieldCustom.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/components/TextFieldCustom.kt
@@ -1,10 +1,14 @@
 package com.example.first_movile_app.ui.components
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.first_movile_app.utils.FormError
+import com.example.first_movile_app.utils.getMessage
 
 @Composable
 fun TextFieldCustom(
@@ -12,15 +16,20 @@ fun TextFieldCustom(
     onValueChange: (String) -> Unit,
     placeholder: String,
     inputLabel: @Composable () -> Unit,
+    errorList: Set<FormError>?,
     modifier: Modifier = Modifier
 ) {
-    TextField(
-        value = value,
-        onValueChange = onValueChange,
-        placeholder = { placeholder },
-        label = inputLabel,
-        modifier = modifier.fillMaxWidth()
-    )
+    Column {
+        TextField(
+            value = value,
+            onValueChange = onValueChange,
+            placeholder = { placeholder },
+            label = inputLabel,
+            modifier = modifier.fillMaxWidth()
+        )
+        errorList?.forEach { it -> Text(getMessage(it)) }
+
+    }
 }
 
 @Preview
@@ -31,6 +40,7 @@ fun TextFieldCustomPreview() {
         onValueChange = {},
         placeholder = "Es un input de prueba",
         inputLabel = { InputLabel("Test label") },
+        errorList = emptySet(),
         Modifier
     )
 }
