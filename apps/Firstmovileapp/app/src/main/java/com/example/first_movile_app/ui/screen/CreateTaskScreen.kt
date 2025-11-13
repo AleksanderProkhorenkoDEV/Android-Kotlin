2a03d5d80794b6b124743db45ad98ad95d48071b
Aleksander <aprokhorenkodev@gmail.com>
2025-11-13 15:14:01 +0100

    feat: add snackbar notification

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/screen/CreateTaskScreen.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/screen/CreateTaskScreen.kt
@@ -1,5 +1,6 @@
 package com.example.first_movile_app.ui.screen
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -7,9 +8,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,52 +31,66 @@ import com.example.first_movile_app.ui.components.InputLabel
 import com.example.first_movile_app.ui.components.TextFieldCustom
 import com.example.first_movile_app.viewModel.CreateTaskViewModel
 import com.example.first_movile_app.viewModel.ViewModalContainer
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun CreateTaskScreen(
     modifier: Modifier = Modifier,
     viewModel: CreateTaskViewModel = viewModel(factory = ViewModalContainer.Factory)
 ) {
     val uiState = viewModel.uiState.collectAsState()
+    val snackbarHostState = remember { SnackbarHostState() }
 
-    Box(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(16.dp)
+    LaunchedEffect(true) {
+        viewModel.snackbarMessage.collect{ message ->
+            snackbarHostState.showSnackbar(message)
+        }
+    }
+
+    Scaffold(
+        snackbarHost = { SnackbarHost(snackbarHostState) },
     ) {
-        Text(text = stringResource(R.string.create_task_title_form))
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(16.dp)
         ) {
-            TextFieldCustom(
-                value = uiState.value.name,
-                onValueChange = { newName -> viewModel.onChangeName(newName) },
-                placeholder = stringResource(R.string.create_form_name_placeholder),
-                inputLabel = { InputLabel(value = stringResource(R.string.create_form_name_label)) },
-                errorList = uiState.value.nameError
-                )
-            Spacer(modifier = Modifier.padding(16.dp))
-            TextFieldCustom(
-                value = uiState.value.description,
-                onValueChange = { newDescription -> viewModel.onChangeDescription(newDescription) },
-                placeholder = stringResource(R.string.create_form_description_placeholder),
-                inputLabel = { InputLabel(value = stringResource(R.string.create_form_description_label)) },
-                errorList = uiState.value.descriptionError
-            )
-            Spacer(modifier = Modifier.padding(16.dp))
-            Button(
-                onClick = { viewModel.saveTask() },
-                enabled = !uiState.value.isLoading
+            Text(text = stringResource(R.string.create_task_title_form))
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
             ) {
-                if(uiState.value.isLoading){
-                    Text(text = stringResource(R.string.loading_button))
-                }else{
-                    Text(text = stringResource(R.string.create_task_button))
+                TextFieldCustom(
+                    value = uiState.value.name,
+                    onValueChange = { newName -> viewModel.onChangeName(newName) },
+                    placeholder = stringResource(R.string.create_form_name_placeholder),
+                    inputLabel = { InputLabel(value = stringResource(R.string.create_form_name_label)) },
+                    errorList = uiState.value.nameError
+                )
+                Spacer(modifier = Modifier.padding(16.dp))
+                TextFieldCustom(
+                    value = uiState.value.description,
+                    onValueChange = { newDescription -> viewModel.onChangeDescription(newDescription) },
+                    placeholder = stringResource(R.string.create_form_description_placeholder),
+                    inputLabel = { InputLabel(value = stringResource(R.string.create_form_description_label)) },
+                    errorList = uiState.value.descriptionError
+                )
+                Spacer(modifier = Modifier.padding(16.dp))
+                Button(
+                    onClick = { viewModel.saveTask() },
+                    enabled = !uiState.value.isLoading
+                ) {
+                    if (uiState.value.isLoading) {
+                        Text(text = stringResource(R.string.loading_button))
+                    } else {
+                        Text(text = stringResource(R.string.create_task_button))
+                    }
                 }
             }
-        }
 
+        }
     }
 }
 
