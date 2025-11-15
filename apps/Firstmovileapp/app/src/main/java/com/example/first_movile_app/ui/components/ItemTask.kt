3adb396f567a9a64ae61c22eff3dee01ef1b8715
aleksander <aprokhorenkodev@gmail.com>
2025-11-15 21:15:43 +0100

    style: delete unused imports

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/components/ItemTask.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/components/ItemTask.kt
@@ -9,9 +9,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,8 +29,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.first_movile_app.R
 import com.example.first_movile_app.dataBase.entities.Task
 
 @Composable
@@ -73,8 +83,39 @@ fun ItemTask(
                 modifier = Modifier
                             .padding(8.dp),
             ) {
+                HorizontalDivider(thickness = 2.dp)
+                Spacer(modifier = Modifier.padding(vertical = 4.dp))
                 Text(task.description)
-                Spacer(modifier = Modifier.height(8.dp))
+                Spacer(modifier = Modifier.padding(vertical = 4.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Button(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Default.Edit,
+                            contentDescription = stringResource(R.string.edit_task),
+                            modifier = Modifier.size(16.dp)
+                        )
+                        Spacer(modifier = Modifier.padding(horizontal = 2.dp))
+                        Text(text = stringResource(R.string.edit_task))
+                    }
+                    Spacer(modifier = Modifier.padding(2.dp))
+                    Button(
+                        onClick = {},
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Red,
+                        )
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Delete,
+                            contentDescription = stringResource(R.string.delete_task),
+                            modifier = Modifier.size(16.dp)
+                        )
+                        Spacer(modifier = Modifier.padding(2.dp))
+                        Text(text = stringResource(R.string.delete_task))
+
+                    }
+                }
             }
         }
     }
