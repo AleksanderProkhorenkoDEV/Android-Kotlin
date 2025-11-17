84db11a9319983e8ebb99661a39756abeaab04a7
Aleksander <aprokhorenkodev@gmail.com>
2025-11-17 14:07:03 +0100

    feat: add text to test the navigation

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/screen/EditTaskScreen.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/screen/EditTaskScreen.kt
@@ -1,8 +1,9 @@
 package com.example.first_movile_app.ui.screen
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun EditTaskScreen(id: Int?){
-
+    Text("Edit task, id: $id")
 }
