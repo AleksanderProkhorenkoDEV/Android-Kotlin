ec75e77dbf2fd0716d21fe7f947fd2142b9d66a9
Aleksander <aprokhorenkodev@gmail.com>
2025-11-18 16:13:44 +0100

    feat: create viewModel to gestionate the edit process

---
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/viewModel/EditTaskViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.first_movile_app.viewModel
+
+import androidx.lifecycle.ViewModel
+import com.example.first_movile_app.dataBase.entities.Task
+import com.example.first_movile_app.dataBase.repositories.TaskRepository
+import kotlinx.coroutines.flow.StateFlow
+
+class EditTaskViewModel(
+    private val taskRepository: TaskRepository,
+): ViewModel()
+{
+
+    private val _taskId = Mutabe
+}
