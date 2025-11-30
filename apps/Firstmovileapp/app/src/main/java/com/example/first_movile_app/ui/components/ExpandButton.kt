2a9af53e4597826b07bb3a69284045e912a2ef2b
Aleksander <aprokhorenkodev@gmail.com>
2025-11-30 17:33:39 +0100

    feat: change the color of the icon

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/components/ExpandButton.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/components/ExpandButton.kt
@@ -6,7 +6,9 @@ import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.first_movile_app.R
 
 @Composable
 fun ExpandButton(
@@ -18,12 +20,13 @@ fun ExpandButton(
     ){
         Icon(
             imageVector = if(isExpanded) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
-            contentDescription = "Expand"
+            contentDescription = "Expand",
+            tint = colorResource(R.color.text_primary)
         )
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = false)
 @Composable
 fun ExpandButtonPreview(){
     ExpandButton(callbackExpanded = {})
