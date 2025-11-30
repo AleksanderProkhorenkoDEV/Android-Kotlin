9eb5ceda348e120623a44e7649027ff2b1ed159a
Aleksander <aprokhorenkodev@gmail.com>
2025-11-30 17:33:18 +0100

    feat: add style to the background and change color

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/components/BottomBar.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/components/BottomBar.kt
@@ -1,16 +1,24 @@
 package com.example.first_movile_app.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.first_movile_app.R
 import com.example.first_movile_app.navigation.TaskDestination
 import com.example.first_movile_app.navigation.taskDestinationBottomBar
 
@@ -23,7 +31,14 @@ fun BottomBar(
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .padding(vertical = 8.dp),
+            .height(48.dp)
+            .background(
+                color = colorResource(R.color.dark_secondary),
+                shape = RoundedCornerShape(
+                topStart = 16.dp,
+                topEnd = 16.dp,
+                bottomStart = 0.dp,
+                bottomEnd = 0.dp)),
         horizontalArrangement = Arrangement.SpaceAround,
         verticalAlignment = Alignment.CenterVertically
     ) {
@@ -33,14 +48,15 @@ fun BottomBar(
             ) {
                 Icon(
                     imageVector = screen.icon,
-                    contentDescription = screen.title
+                    contentDescription = screen.title,
+                    tint = colorResource(R.color.text_primary)
                 )
             }
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun BottomBarPreview() {
     BottomBar(allScreens = taskDestinationBottomBar, {})
