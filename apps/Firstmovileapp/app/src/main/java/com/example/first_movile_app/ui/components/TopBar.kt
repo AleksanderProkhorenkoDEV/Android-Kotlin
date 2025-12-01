5e4df415467f9af7ee6920b5c358878cd59e137b
Aleksander <aprokhorenkodev@gmail.com>
2025-12-01 15:04:26 +0100

    feat: add shadow and add space

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/components/TopBar.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/components/TopBar.kt
@@ -1,11 +1,15 @@
 package com.example.first_movile_app.ui.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -16,6 +20,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,19 +33,22 @@ fun TopBar(
     onNavigationBack: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val shape = RoundedCornerShape(
+        topStart = 0.dp,
+        topEnd = 0.dp,
+        bottomStart = 20.dp,
+        bottomEnd = 20.dp
+    )
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .height(24.dp)
+            .height(48.dp)
+            .shadow(elevation = 10.dp, shape = shape)
             .background(
                 color = colorResource(R.color.surface),
-                shape = RoundedCornerShape(
-                    topStart = 0.dp,
-                    topEnd = 0.dp,
-                    bottomStart = 20.dp,
-                    bottomEnd = 20.dp
-                )
-            ),
+                shape = shape
+            )
+            .padding(8.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         IconButton(
@@ -50,7 +58,7 @@ fun TopBar(
             Icon(
                 imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                 contentDescription = "go back",
-                modifier = Modifier.size(16.dp)
+                modifier = Modifier.size(24.dp)
             )
         }
         Text(
