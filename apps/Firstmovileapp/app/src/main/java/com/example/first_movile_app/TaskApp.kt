ab4f8aa8df3cf7f19bdb7084bdf1616cab5c9088
Aleksander <aprokhorenkodev@gmail.com>
2025-12-03 14:17:04 +0100

    feat: start to change the top bar

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/TaskApp.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/TaskApp.kt
@@ -5,36 +5,47 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.first_movile_app.ui.theme.FirstmovileappTheme
 import com.example.first_movile_app.navigation.TaskNavHost
 import com.example.first_movile_app.navigation.taskDestinationBottomBar
 import com.example.first_movile_app.ui.components.BottomBar
 
+@OptIn(ExperimentalMaterial3Api::class)
 @RequiresApi(Build.VERSION_CODES.O)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun TaskApp() {
     FirstmovileappTheme {
         val navController = rememberNavController()
+        //Investigate to TopAppBar and change it, same with bottom
         Scaffold(
             modifier = Modifier.fillMaxSize(),
+            topBar = {
+                TopAppBar(
+                    title = { Text(text = "Titulo de la screen") }
+                )
+            },
             bottomBar = {
                 BottomBar(
                     allScreens = taskDestinationBottomBar,
                     onBottomSelected = { newScreen ->
-                        navController.navigate(route = newScreen.route){
+                        navController.navigate(route = newScreen.route) {
                             launchSingleTop = true
                         }
                     }
                 )
             },
-            containerColor = colorResource(R.color.primary),
-            contentColor = colorResource(R.color.text_primary)
+            containerColor = colorResource(R.color.surface),
         ) { innerPadding ->
             TaskNavHost(
                 navController = navController,
