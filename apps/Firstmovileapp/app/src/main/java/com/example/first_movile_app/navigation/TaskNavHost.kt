360ef79c7ff550792043c22c2cb3f63b62d60f62
aleksander <aprokhorenkodev@gmail.com>
2025-12-20 12:42:16 +0100

    feat: change the name of the title

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/navigation/TaskNavHost.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/navigation/TaskNavHost.kt
@@ -3,21 +3,27 @@ package com.example.first_movile_app.navigation
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.toRoute
 import com.example.first_movile_app.ui.screen.AccountSettingsScreen
 import com.example.first_movile_app.ui.screen.CreateTaskScreen
 import com.example.first_movile_app.ui.screen.EditTaskScreen
 import com.example.first_movile_app.ui.screen.MainScreen
+import com.example.first_movile_app.viewModel.TopAppBarViewModel
+import com.example.first_movile_app.viewModel.ViewModalContainer
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.first_movile_app.R
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun TaskNavHost(
     navController: NavHostController,
-    modifier: Modifier
+    modifier: Modifier,
+    viewModel: TopAppBarViewModel = viewModel(factory = ViewModalContainer.Factory)
 ) {
     NavHost(
         navController = navController,
@@ -25,11 +31,13 @@ fun TaskNavHost(
         modifier = modifier,
     ) {
         composable<TaskList> {
+            viewModel.setTitle(stringResource(R.string.app_name))
             MainScreen(onNavigateToEditScreen = { id ->
                 navController.navigate(EditTask(id = id))
             })
         }
         composable<CreateTask> {
+            viewModel.setTitle(stringResource(R.string.create_task_title_form))
             CreateTaskScreen(
                 onNavigationBack = {
                     navController.popBackStack()
@@ -39,12 +47,14 @@ fun TaskNavHost(
                 })
         }
         composable<AccountSettings> {
+            viewModel.setTitle(stringResource(R.string.setting_page))
             AccountSettingsScreen(onNavigationBack = {
                 navController.popBackStack()
             })
         }
 
         composable<EditTask> { backStackEntry ->
+            viewModel.setTitle(stringResource(R.string.edit_task))
             EditTaskScreen(
                 onNavigationBack = {
                     navController.popBackStack()
