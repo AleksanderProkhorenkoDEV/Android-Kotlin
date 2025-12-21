29024424d69d6c33ab9b6ba5d9d79dfa185baf14
Aleksander <aprokhorenkodev@gmail.com>
2025-12-21 13:28:36 +0100

    refactor: delete the scafold

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/screen/AccountSettingsScreen.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/screen/AccountSettingsScreen.kt
@@ -1,8 +1,10 @@
 package com.example.first_movile_app.ui.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -10,14 +12,12 @@ import androidx.compose.ui.res.stringResource
 import com.example.first_movile_app.R
 
 @Composable
-fun AccountSettingsScreen (onNavigationBack: () -> Unit) {
-    Scaffold(
-
-    ) { innerPadding ->
-        Column(
-            modifier = Modifier.padding(innerPadding)
-        ) {
-            Text("Personalize your settings")
-        }
+fun AccountSettingsScreen(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text("Personalize your settings")
     }
 }
