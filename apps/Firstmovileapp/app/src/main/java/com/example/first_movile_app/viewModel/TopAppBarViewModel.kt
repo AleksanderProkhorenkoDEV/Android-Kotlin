e2f476d787406bc792c771244e78ffb0cf4071ed
Aleksander <aprokhorenkodev@gmail.com>
2025-12-21 11:19:30 +0100

    feat: add data class and make generic

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/viewModel/TopAppBarViewModel.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/viewModel/TopAppBarViewModel.kt
@@ -4,12 +4,19 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+
+data class TopAppBarState(
+    val title: String = "",
+    val canNavigateBack: Boolean = false
+)
 
 class TopAppBarViewModel() : ViewModel() {
-    private val _title: MutableStateFlow<String> = MutableStateFlow("Task App")
-    val title: StateFlow<String> = _title.asStateFlow()
+    private val _uiState: MutableStateFlow<TopAppBarState> = MutableStateFlow(TopAppBarState())
+    val uiState: StateFlow<TopAppBarState> = _uiState.asStateFlow()
 
-    fun setTitle(title: String) {
-        _title.value = title
+    fun updateTopAppBar(title: String, canNavigateBack: Boolean) {
+        _uiState.update { it.copy(title = title, canNavigateBack = canNavigateBack) }
     }
 }
