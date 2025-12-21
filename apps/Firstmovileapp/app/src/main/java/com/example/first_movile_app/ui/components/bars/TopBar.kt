dc8bd2b32373274d0a43756147f538f0706eb638
Aleksander <aprokhorenkodev@gmail.com>
2025-12-21 13:36:52 +0100

    feat: customize the aparience

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/components/bars/TopBar.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/components/bars/TopBar.kt
@@ -1,5 +1,8 @@
 package com.example.first_movile_app.ui.components.bars
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -7,7 +10,12 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.first_movile_app.R
 import com.example.first_movile_app.ui.components.buttons.ActionButton
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -17,8 +25,23 @@ fun TopBar(
     showBackButton: Boolean,
     onNavigationBack: () -> Unit,
 ) {
+
+    val shape = RoundedCornerShape(
+        topStart = 0.dp,
+        topEnd = 0.dp,
+        bottomStart = 20.dp,
+        bottomEnd = 20.dp
+    )
+
     TopAppBar(
         title = { Text(title) },
+        modifier = Modifier
+            .shadow(elevation = 10.dp, shape = shape)
+            .background(
+                color = colorResource(R.color.secondary),
+                shape = shape
+            )
+            .fillMaxWidth(),
         navigationIcon = {
             if (showBackButton) {
                 ActionButton(
@@ -39,5 +62,5 @@ fun TopBar(
 @Preview()
 @Composable
 fun TopBarPreview() {
-    TopBar(title = "Tareas", showBackButton = false ,onNavigationBack = {})
+    TopBar(title = "Tareas", showBackButton = false, onNavigationBack = {})
 }
