3d1775ab839e0286f501cc5b822db3c94ad59574
Aleksander <aprokhorenkodev@gmail.com>
2025-12-21 13:49:32 +0100

    fix: error with unused import directives and parametres

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/screen/EditTaskScreen.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/screen/EditTaskScreen.kt
@@ -2,22 +2,17 @@ package com.example.first_movile_app.ui.screen
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
@@ -33,7 +28,6 @@ import com.example.first_movile_app.viewModel.ViewModalContainer
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun EditTaskScreen(
-    modifier: Modifier = Modifier,
     snackbarHostState: SnackbarHostState,
     onNavigationList: () -> Unit,
     viewModel: EditTaskViewModel = viewModel(factory = ViewModalContainer.Factory)
