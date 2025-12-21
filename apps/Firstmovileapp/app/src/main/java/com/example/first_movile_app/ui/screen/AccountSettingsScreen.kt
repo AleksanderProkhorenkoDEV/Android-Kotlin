0558bad876a1c04496b8e945421a39fa3a80eece
Aleksander <aprokhorenkodev@gmail.com>
2025-12-21 13:29:00 +0100

    style: delete un used imports

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/screen/AccountSettingsScreen.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/screen/AccountSettingsScreen.kt
@@ -2,14 +2,10 @@ package com.example.first_movile_app.ui.screen
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import com.example.first_movile_app.R
+
 
 @Composable
 fun AccountSettingsScreen(
