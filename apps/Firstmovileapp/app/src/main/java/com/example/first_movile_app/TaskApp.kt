7e6ad746b40fa8f0464fab5c53fb251129dd53a6
Aleksander <aprokhorenkodev@gmail.com>
2025-12-21 11:19:09 +0100

    feat: make dynamic the topappbar and bottombar

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/TaskApp.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/TaskApp.kt
@@ -7,13 +7,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
@@ -23,41 +23,74 @@ import com.example.first_movile_app.navigation.EditTask
 import com.example.first_movile_app.navigation.TaskList
 import com.example.first_movile_app.ui.theme.FirstmovileappTheme
 import com.example.first_movile_app.navigation.TaskNavHost
+import com.example.first_movile_app.navigation.isTopLevelRoute
 import com.example.first_movile_app.navigation.taskDestinationBottomBar
 import com.example.first_movile_app.ui.components.bars.BottomBar
 import com.example.first_movile_app.ui.components.bars.TopBar
 import com.example.first_movile_app.viewModel.TopAppBarViewModel
 import com.example.first_movile_app.viewModel.ViewModalContainer
 
+
 @OptIn(ExperimentalMaterial3Api::class)
 @RequiresApi(Build.VERSION_CODES.O)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun TaskApp(
+    viewModel: TopAppBarViewModel = viewModel(factory = ViewModalContainer.Factory)
 ) {
     FirstmovileappTheme {
         val navController = rememberNavController()
 
+        //Get the state of navigation
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        val showBottomBar = currentRoute.isTopLevelRoute()
+
+        val appBarState = remember(currentRoute) {
+            val title = when {
+                currentRoute?.contains(TaskList::class.simpleName.toString()) == true -> "Mis Tareas"
+                currentRoute?.contains(AccountSettings::class.simpleName.toString()) == true -> "Ajustes"
+                currentRoute?.contains(CreateTask::class.simpleName.toString()) == true -> "Crear Tarea"
+                currentRoute?.contains(EditTask::class.simpleName.toString()) == true -> "Editar Tarea"
+                else -> ""
+            }
+            val canNavigateBack =
+                !currentRoute.isTopLevelRoute() && navController.previousBackStackEntry != null
+            title to canNavigateBack
+        }
+
+        val (title, canNavigateBack) = appBarState
+        val snackbar = remember { SnackbarHostState() }
+
         Scaffold(
             modifier = Modifier.fillMaxSize(),
             topBar = {
-
-            },
-            bottomBar = {
-                BottomBar(
-                    allScreens = taskDestinationBottomBar,
-                    onBottomSelected = { newScreen ->
-                        navController.navigate(route = newScreen.route) {
-                            launchSingleTop = true
-                        }
-                    }
+                TopBar(
+                    title = title,
+                    showBackButton = !currentRoute.isTopLevelRoute(),
+                    onNavigationBack = { navController.popBackStack() }
                 )
             },
+            bottomBar = {
+                if (showBottomBar) {
+                    BottomBar(
+                        allScreens = taskDestinationBottomBar,
+                        onBottomSelected = { newScreen ->
+                            navController.navigate(route = newScreen.route) {
+                                launchSingleTop = true
+                            }
+                        }
+                    )
+                }
+            },
+            snackbarHost = { SnackbarHost(snackbar) },
             containerColor = colorResource(R.color.surface),
         ) { innerPadding ->
             TaskNavHost(
                 navController = navController,
-                modifier = Modifier.padding(innerPadding)
+                modifier = Modifier.padding(innerPadding),
+                snackbarHostState = snackbar,
             )
         }
     }
