bdf240da74217046396100ca15f32e64001bd70b
aleksander <aprokhorenkodev@gmail.com>
2025-12-22 19:57:06 +0100

    feat: defined the new color pattern of the app

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/theme/Color.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/theme/Color.kt
@@ -1,11 +1,16 @@
 package com.example.first_movile_app.ui.theme
 
 import androidx.compose.ui.graphics.Color
+// Colores para el tema claro
+val LightText = Color(0xFF040316)
+val LightBackground = Color(0xFFFBFBFE)
+val LightPrimary = Color(0xFF2F27CE)
+val LightSecondary = Color(0xFFDDDBFF)
+val LightAccent = Color(0xFF443DFF)
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Colores para el tema oscuro
+val DarkText = Color(0xFFEAE9FC)
+val DarkBackground = Color(0xFF010104)
+val DarkPrimary = Color(0xFF3A31D8)
+val DarkSecondary = Color(0xFF020024)
+val DarkAccent = Color(0xFF0600C2)
