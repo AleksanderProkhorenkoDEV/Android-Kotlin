17b1aa61f9fca816dc23d976a3c64841838be856
aleksander <aprokhorenkodev@gmail.com>
2025-12-23 12:59:11 +0100

    feat: start to personalizate the screen and add toggle button

--- apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/screen/AccountSettingsScreen.kt
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/ui/screen/AccountSettingsScreen.kt
@@ -1,18 +1,41 @@
 package com.example.first_movile_app.ui.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.first_movile_app.viewModel.SettingsViewModel
+import com.example.first_movile_app.viewModel.ViewModalContainer
 
 
 @Composable
 fun AccountSettingsScreen(
+    isDarkTheme: Boolean,
+    onToggleTheme: () -> Unit,
 ) {
-    Column(
-        modifier = Modifier.fillMaxWidth()
-    ) {
+
+    Column(modifier = Modifier.fillMaxWidth()) {
         Text("Personalize your settings")
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = if (isDarkTheme) "Modo oscuro" else "Modo claro",
+                modifier = Modifier.weight(1f)
+            )
+            Switch(
+                checked = isDarkTheme,
+                onCheckedChange = { onToggleTheme() }
+            )
+        }
     }
 }
