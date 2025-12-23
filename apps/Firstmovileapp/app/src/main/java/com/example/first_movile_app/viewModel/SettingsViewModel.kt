ce499cab8b57db9e3b95f7a3ceaca37f4d2e6605
aleksander <aprokhorenkodev@gmail.com>
2025-12-23 12:58:40 +0100

    feat: Create viewModel to manage the color of the theme

---
+++ apps/Firstmovileapp/app/src/main/java/com/example/first_movile_app/viewModel/SettingsViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.first_movile_app.viewModel
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class SettingsViewModel : ViewModel() {
+    private val _isDarkTheme = MutableStateFlow(false)
+    val isDarkTheme = _isDarkTheme.asStateFlow()
+
+    fun toggleTheme() {
+        _isDarkTheme.value = !_isDarkTheme.value
+    }
+
+}
