76a58b6ae5c7737deebbced93d4ae45554c07976
aleksander <aprokhorenkodev@gmail.com>
2026-01-03 16:17:03 +0100

    feat: create the factory and function to know the context to add db in the future

---
+++ apps/AuthApp/app/src/main/java/com/example/authapp/viewModels/ViewModelContainer.kt
@@ -0,0 +1,18 @@
+package com.example.authapp.viewModels
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.authapp.AuthApplication
+
+object ViewModelContainer{
+    val Factory = viewModelFactory {
+        initializer<AuthViewModel> {
+            AuthViewModel()
+        }
+    }
+}
+
+fun CreationExtras.authApplication(): AuthApplication =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AuthApplication)
