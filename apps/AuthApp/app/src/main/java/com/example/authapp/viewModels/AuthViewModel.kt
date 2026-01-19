89b0a82ca7ffb7145450bc667cb75ff8dd71cf27
Aleksander <aprokhorenkodev@gmail.com>
2026-01-19 14:05:19 +0100

    Viewmodel | feat: create states to represent auth session, and add two functions without body

--- apps/AuthApp/app/src/main/java/com/example/authapp/viewModels/AuthViewModel.kt
+++ apps/AuthApp/app/src/main/java/com/example/authapp/viewModels/AuthViewModel.kt
@@ -1,7 +1,22 @@
 package com.example.authapp.viewModels
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+sealed class AuthState{
+    object Unauthenticated : AuthState()
+    object Loading: AuthState()
+    data class Authenticated(val user: User): AuthState()
+    data class Error(val message: String): AuthState()
+}
 
 class AuthViewModel: ViewModel() {
+    private val _authState = MutableStateFlow<AuthState>(AuthState.Unauthenticated)
+    val authState: StateFlow<AuthState> = _authState.asStateFlow()
 
+    fun login(){}
+
+    fun logout(){}
 }
