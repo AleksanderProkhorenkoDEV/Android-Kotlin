af59d997927ea5b62e5d14a3599c83840300a5a5
Aleksander <aprokhorenkodev@gmail.com>
2026-01-19 14:21:17 +0100

    Screens | feat: add text to Dashboard Screen

--- apps/AuthApp/app/src/main/java/com/example/authapp/ui/theme/screens/privates/DashboardScreen.kt
+++ apps/AuthApp/app/src/main/java/com/example/authapp/ui/theme/screens/privates/DashboardScreen.kt
@@ -1,6 +1,10 @@
 package com.example.authapp.ui.theme.screens.privates
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
-fun DashboardScreen(){}
+fun DashboardScreen(){
+    Text("Dashboard screen.")
+    Text("Now you are log in")
+}
