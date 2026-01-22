ceadb2b0ea618b1deec76788fb98499ef0b01a38
Aleksander <aprokhorenkodev@gmail.com>
2026-01-22 11:20:51 +0100

    UI | feat: create column to display inputs, and customize the screen

--- apps/AuthApp/app/src/main/java/com/example/authapp/ui/theme/screens/publics/RegisterScreen.kt
+++ apps/AuthApp/app/src/main/java/com/example/authapp/ui/theme/screens/publics/RegisterScreen.kt
@@ -1,24 +1,89 @@
 package com.example.authapp.ui.theme.screens.publics
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.authapp.R
 
 @Composable
 fun RegisterScreen(
-    callbackNavigationToLogin: () -> Unit
+    callbackNavigationToLogin: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
-    Column() {
-        Text(text = "Register Screen")
+    Column(
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.login),
+                contentDescription = "Login Image",
+                modifier = Modifier.size(164.dp, 164.dp)
+            )
+            Text(text = "Register your account")
+        }
+        Column(
+            modifier = modifier
+                .padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            TextField(
+                value = "",
+                onValueChange = {},
+                label = { Text("Write your username")},
+                placeholder = { Text("Ej: Mathew")},
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                modifier = Modifier.fillMaxWidth()
+            )
+            TextField(
+                value = "",
+                onValueChange = {},
+                label = { Text("Write email")},
+                placeholder = { Text("Ej: mathew@gmail.com")},
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                modifier = Modifier.fillMaxWidth()
+            )
+            TextField(
+                value = "",
+                onValueChange = {},
+                label = { Text("Write your password")},
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
         Button(onClick = callbackNavigationToLogin) {
             Text(text = "Sign In")
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun RegisterScreenPreview() {
     RegisterScreen(
