a8c58f79e0b23bd1cc2a1de46572c8a1ea7cf70e
Aleksander <aprokhorenkodev@gmail.com>
2026-01-25 15:14:06 +0100

    SCREEN | feat: add new input (confirm password) and checkbox to garantize the user accept the terms and conditions

--- apps/AuthApp/app/src/main/java/com/example/authapp/ui/theme/screens/publics/RegisterScreen.kt
+++ apps/AuthApp/app/src/main/java/com/example/authapp/ui/theme/screens/publics/RegisterScreen.kt
@@ -1,6 +1,7 @@
 package com.example.authapp.ui.theme.screens.publics
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,22 +13,30 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.authapp.R
+import com.example.authapp.ui.viewModels.AuthViewModel
+import com.example.authapp.ui.viewModels.ViewModelContainer
 
 @Composable
 fun RegisterScreen(
     callbackNavigationToLogin: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: AuthViewModel = viewModel(factory = ViewModelContainer.Factory)
 ) {
+
     Column(
         modifier = modifier
             .padding(16.dp)
@@ -55,29 +64,72 @@ fun RegisterScreen(
             TextField(
                 value = "",
                 onValueChange = {},
-                label = { Text("Write your username")},
-                placeholder = { Text("Ej: Mathew")},
+                label = { Text("Write your username") },
+                placeholder = { Text("Ej: Mathew") },
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
                 modifier = Modifier.fillMaxWidth()
             )
             TextField(
                 value = "",
                 onValueChange = {},
-                label = { Text("Write email")},
-                placeholder = { Text("Ej: mathew@gmail.com")},
+                label = { Text("Write email") },
+                placeholder = { Text("Ej: mathew@gmail.com") },
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
                 modifier = Modifier.fillMaxWidth()
             )
             TextField(
                 value = "",
                 onValueChange = {},
-                label = { Text("Write your password")},
+                label = { Text("Write your password") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                modifier = Modifier.fillMaxWidth()
+            )
+            TextField(
+                value = "",
+                onValueChange = {},
+                label = { Text("Confirm your password") },
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
                 modifier = Modifier.fillMaxWidth()
             )
         }
-        Button(onClick = callbackNavigationToLogin) {
-            Text(text = "Sign In")
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(
+                checked = false,
+                onCheckedChange = {}
+            )
+            Text(
+                text = "I agree to the terms and conditions"
+            )
+        }
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxSize(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                text = "Do you have an account?"
+            )
+            Text(
+                text = "Sign In",
+                style = TextStyle(color = Color.Blue),
+                modifier = Modifier.clickable {
+                    callbackNavigationToLogin()
+                }
+            )
+        }
+        Button(
+            onClick = {
+                viewModel.register()
+            }
+        ) {
+            Text(
+                text = "Register Account"
+            )
         }
     }
 }
