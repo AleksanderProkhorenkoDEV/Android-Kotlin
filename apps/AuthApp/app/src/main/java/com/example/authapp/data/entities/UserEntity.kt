a859b8ca097a5e8e8e9089cfe368f73c166ae3b9
Aleksander <aprokhorenkodev@gmail.com>
2026-01-25 13:01:39 +0100

    DATABASE | feat: create user Entity

---
+++ apps/AuthApp/app/src/main/java/com/example/authapp/data/entities/UserEntity.kt
@@ -0,0 +1,12 @@
+package com.example.authapp.data.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+@Entity(tableName = "users")
+data class UserEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null,
+    val name: String,
+    val email: String,
+    val password: String
+)
