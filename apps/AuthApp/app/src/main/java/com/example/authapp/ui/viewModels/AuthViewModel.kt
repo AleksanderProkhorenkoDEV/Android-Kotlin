9b0ab3c9c30e9b26f369c06f071312b76002478d
Aleksander <aprokhorenkodev@gmail.com>
2026-01-26 14:08:01 +0100

    VIEWMODEL | feat: add function to use in onchange method, and mutableStateFlow

--- apps/AuthApp/app/src/main/java/com/example/authapp/ui/viewModels/AuthViewModel.kt
+++ apps/AuthApp/app/src/main/java/com/example/authapp/ui/viewModels/AuthViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.authapp.ui.viewModels
 
 import androidx.lifecycle.ViewModel
+import com.example.authapp.ui.forms.RegisterForm
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -15,6 +16,13 @@ sealed class AuthState{
 class AuthViewModel: ViewModel() {
     private val _authState = MutableStateFlow<AuthState>(AuthState.Unauthenticated)
     val authState: StateFlow<AuthState> = _authState.asStateFlow()
+
+    private val _registerFormState = MutableStateFlow<RegisterForm>(RegisterForm())
+    val registerState: StateFlow<RegisterForm> = _registerFormState.asStateFlow()
+
+    fun updateRegisterForm(newForm: RegisterForm) {
+        _registerFormState.value = newForm
+    }
 
     fun login(email: String, password: String){
     }
