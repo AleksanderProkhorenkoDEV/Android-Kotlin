2c5a5dc294ece37d4299f8141c921cb3aaaa6d62
Aleksander <aprokhorenkodev@gmail.com>
2026-01-28 13:40:29 +0100

    DAO | feat: add insert function

--- apps/AuthApp/app/src/main/java/com/example/authapp/data/dao/UserDao.kt
+++ apps/AuthApp/app/src/main/java/com/example/authapp/data/dao/UserDao.kt
@@ -1,6 +1,8 @@
 package com.example.authapp.data.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.authapp.data.entities.UserEntity
 
@@ -8,4 +10,9 @@ import com.example.authapp.data.entities.UserEntity
 interface UserDao {
     @Query("SELECT * FROM users")
     fun getAll(): List<UserEntity>
+
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(user: UserEntity): Long
+
 }
