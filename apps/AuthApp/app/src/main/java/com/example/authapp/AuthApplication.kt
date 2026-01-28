18c6ee699ce59c8f0bb94deb86287ea791dd0a06
Aleksander <aprokhorenkodev@gmail.com>
2026-01-28 13:43:17 +0100

    APP | feat: add container, to permit pass the repository

--- apps/AuthApp/app/src/main/java/com/example/authapp/AuthApplication.kt
+++ apps/AuthApp/app/src/main/java/com/example/authapp/AuthApplication.kt
@@ -1,6 +1,10 @@
 package com.example.authapp
 
 import android.app.Application
+import com.example.authapp.di.AppDataContainer
 
 class AuthApplication : Application() {
+    val container by lazy {
+        AppDataContainer(this)
+    }
 }
