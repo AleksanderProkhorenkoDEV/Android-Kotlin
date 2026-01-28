0e926f0efd84aaea3fc8f855b7b843cf6fae3cc2
Aleksander <aprokhorenkodev@gmail.com>
2026-01-28 13:40:10 +0100

    REPOSITORY | feat: create register function

--- apps/AuthApp/app/src/main/java/com/example/authapp/data/repositories/UserRepository.kt
+++ apps/AuthApp/app/src/main/java/com/example/authapp/data/repositories/UserRepository.kt
@@ -1,13 +1,31 @@
 package com.example.authapp.data.repositories
 
-import android.content.Context
-import com.example.authapp.data.database.AppDataBase
+import com.example.authapp.data.dao.ResultDao
+import com.example.authapp.data.dao.UserDao
+import com.example.authapp.data.entities.UserEntity
+import org.mindrot.jbcrypt.BCrypt
 
-class UserRepository(private val context: Context) {
+class UserRepository(
+    private val userDao: UserDao
+) {
+    suspend fun register(
+        name: String,
+        email: String,
+        password: String,
+    ): ResultDao {
+        val userEntity = UserEntity(
+            name = name,
+            email = email,
+            password = hashPassword(password)
+        )
+        val result = userDao.insert(userEntity)
 
-    private val database by lazy {
-        AppDataBase.getDataBase(context)
+        if(result == -1L) return ResultDao.Error("User already exists")
+
+        return ResultDao.Success(userEntity.copy(id = result.toInt()))
     }
 
-    suspend fun register(){}
+    private fun hashPassword(password: String): String {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
 }
