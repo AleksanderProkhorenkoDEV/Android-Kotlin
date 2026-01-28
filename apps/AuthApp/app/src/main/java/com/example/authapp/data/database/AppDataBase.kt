5a6ef23842161dd443d3384c77d4c5af2c0caa68
Aleksander <aprokhorenkodev@gmail.com>
2026-01-28 13:43:53 +0100

    DATABASE | refactor: use the AppDataBase of the task application

--- apps/AuthApp/app/src/main/java/com/example/authapp/data/database/AppDataBase.kt
+++ apps/AuthApp/app/src/main/java/com/example/authapp/data/database/AppDataBase.kt
@@ -4,30 +4,37 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.authapp.data.dao.TermsDao
 import com.example.authapp.data.dao.UserDao
+import com.example.authapp.data.entities.TermsEntity
 import com.example.authapp.data.entities.UserEntity
 
 @Database(
-    entities = [UserEntity::class],
-    version=1,
-    exportSchema = false)
-abstract class AppDataBase: RoomDatabase() {
+    entities = [UserEntity::class, TermsEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDataBase : RoomDatabase() {
     abstract fun userDao(): UserDao
-
-    companion object{
+    abstract fun termsDao(): TermsDao
+    companion object {
         @Volatile
         private var INSTANCE: AppDataBase? = null
 
-        fun getDataBase(context: Context): AppDataBase{
-            return INSTANCE ?: synchronized(this){
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    AppDataBase::class.java,
-                    "AuthDataBase"
-                ).build()
-                INSTANCE = instance
-                instance
-            }
+
+        fun getInstance(context: Context): AppDataBase = INSTANCE ?: synchronized(this) {
+            INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
         }
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                AppDataBase::class.java,
+                "AuthDataBase.db"
+            )
+                .fallbackToDestructiveMigration(true)
+                .build()
+
+
     }
 }
