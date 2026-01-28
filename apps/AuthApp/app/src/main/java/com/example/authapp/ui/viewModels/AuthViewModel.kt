860fa8f5d8cb163411ee988e74000e2469dd5a71
Aleksander <aprokhorenkodev@gmail.com>
2026-01-28 13:42:55 +0100

    VIEWMODEL | refactor: add the method to register the user

--- apps/AuthApp/app/src/main/java/com/example/authapp/ui/viewModels/AuthViewModel.kt
+++ apps/AuthApp/app/src/main/java/com/example/authapp/ui/viewModels/AuthViewModel.kt
@@ -5,22 +5,31 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.authapp.data.dao.ResultDao
+import com.example.authapp.data.entities.UserEntity
+import com.example.authapp.data.repositories.UserRepository
 import com.example.authapp.ui.forms.ValidationResult
 import com.example.authapp.ui.forms.register.RegisterEvent
 import com.example.authapp.ui.forms.register.RegisterForm
 import com.example.authapp.ui.forms.register.RegisterValidator
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 sealed class AuthState {
     object Unauthenticated : AuthState()
     object Loading : AuthState()
-    data class Authenticated(val user: User) : AuthState()
+    data class Authenticated(val user: UserEntity) : AuthState()
     data class Error(val message: String) : AuthState()
 }
 
-class AuthViewModel : ViewModel() {
+class AuthViewModel(
+    private val userRepository: UserRepository
+) : ViewModel() {
     private val _authState = MutableStateFlow<AuthState>(AuthState.Unauthenticated)
     val authState: StateFlow<AuthState> = _authState.asStateFlow()
 
@@ -29,23 +38,28 @@ class AuthViewModel : ViewModel() {
 
     private val validator = RegisterValidator()
 
-    fun onEvent(event: RegisterEvent){
-        when(event){
+    fun onEvent(event: RegisterEvent) {
+        when (event) {
             is RegisterEvent.NameChanged -> {
                 formState = formState.copy(name = event.name)
             }
+
             is RegisterEvent.EmailChanged -> {
                 formState = formState.copy(email = event.email)
             }
+
             is RegisterEvent.PasswordChanged -> {
                 formState = formState.copy(password = event.password)
             }
+
             is RegisterEvent.ConfirmPasswordChanged -> {
                 formState = formState.copy(confirmPassword = event.confirmPassword)
             }
+
             is RegisterEvent.TermsAndConditionsChanged -> {
                 formState = formState.copy(termsAndConditions = event.termsAndConditions)
             }
+
             is RegisterEvent.Submit -> {
                 register()
             }
@@ -57,7 +71,7 @@ class AuthViewModel : ViewModel() {
 
     fun logout() {}
 
-    fun register() {
+    fun register()  {
         val nameRes = validator.validateName(formState.name)
         val emailRes = validator.validateEmail(formState.email)
         val passRes = validator.validatePassword(formState.password)
@@ -76,7 +90,26 @@ class AuthViewModel : ViewModel() {
 
 
         if (!hasError) {
+            viewModelScope.launch {
+                _authState.value = AuthState.Loading
 
+                val result = userRepository.register(
+                    formState.name,
+                    formState.email,
+                    formState.password
+                )
+
+                when (result) {
+                    is ResultDao.Success -> {
+                        _authState.value = AuthState.Authenticated(result.user)
+                        formState = RegisterForm()
+                    }
+
+                    is ResultDao.Error -> {
+                        _authState.value = AuthState.Error(result.message)
+                    }
+                }
+            }
         }
     }
 }
